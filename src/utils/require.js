daa8b421a3ffeaac2f332a8fece2e576667f0d41
piaoge <2393764821@qq.com>
2019-09-03 21:04:02 +0800

    json-bigint

--- src/utils/require.js
+++ src/utils/require.js
@@ -1,8 +1,17 @@
 import axios from 'axios'
-
+import JSONbig from 'json-bigint'
 const instance = axios.create({
   baseURL: 'http://ttapi.research.itcast.cn'
 })
+instance.defaults.transformResponse = [function (data) {
+  try {
+    // data 数据可能不是标准的 JSON 格式字符串，否则会导致 JSONbig.parse(data) 转换失败报错
+    return JSONbig.parse(data)
+  } catch (err) {
+    // 无法转换的数据直接原样返回
+    return data
+  }
+}]
 
 // Add a request interceptor
 instance.interceptors.request.use(function (config) {
