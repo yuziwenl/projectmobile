c7d3f4710613dc808941481e025fa9c214f1d757
piaoge <2393764821@qq.com>
2019-09-05 19:45:57 +0800

    首页布局和list组件深度作用器

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,27 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Tabbar from '@/views/Tabbar.vue'
+import Home from '@/views/Home/index.vue'
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-
-    {
-      path: '/login',
-      name: 'login',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ '../views/login')
-    }
+  routes: [{
+  // name: 'tabbar',
+    path: '/',
+    component: Tabbar,
+    children: [{
+      name: 'Home',
+      path: '',
+      component: Home
+    }]
+  },
+  {
+    path: '/login',
+    name: 'login',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/login')
+  }
   ]
 })
---
+++ src/views/Home/index.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+     <van-nav-bar
+      title="黑马头条" fixed
+    />
+    <!-- 频道列表 -->
+    <van-tabs animated>
+      <van-tab v-for="index in 8" :title="'标签 ' + index" :key="index">
+            <!-- 文章列表,不同的标签页下有不同的列表 -->
+        <van-list
+          v-model="loading"
+          :finished="finished"
+          finished-text="没有更多了"
+          @load="onLoad"
+        >
+          <van-cell
+            v-for="item in list"
+            :key="item"
+            :title="item"
+          />
+        </van-list>
+      </van-tab>
+    </van-tabs>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      list: [],
+      loading: false,
+      finished: false
+    }
+  },
+  methods: {
+    onLoad () {
+      // 异步更新数据
+      setTimeout(() => {
+        for (let i = 0; i < 10; i++) {
+          this.list.push(this.list.length + 1)
+        }
+        // 加载状态结束
+        this.loading = false
+        // 数据全部加载完成
+        if (this.list.length >= 40) {
+          this.finished = true
+        }
+      }, 500)
+    }
+  }
+}
+</script>
+
+<style lang='less' scoped>
+.van-tabs /deep/ .van-tabs__content{
+  margin-top:46px;
+  margin-bottom:50px;
+}
+
+</style>
