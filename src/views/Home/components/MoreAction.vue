2f0e19c7eb6b962942bd424404a419042b2b32c9
piaoge <2393764821@qq.com>
2019-09-05 22:31:32 +0800

    弹层显示内容

--- src/views/Home/components/MoreAction.vue
+++ src/views/Home/components/MoreAction.vue
@@ -1,12 +1,33 @@
 <template>
-  <div>
-    1213
-  </div>
+ <van-dialog
+    v-model="show"
+    :showConfirmButton="false"
+    closeOnClickOverlay
+  >
+   <van-cell-group v-show="!showReports">
+      <van-cell title="不感兴趣" icon="location-o"/>
+      <van-cell title="反馈垃圾内容" icon="location-o" is-link @click="showReports=true"/>
+      <van-cell title="拉黑作者" icon="location-o"/>
+    </van-cell-group>
+    <!-- 举报文章 -->
+    <van-cell-group v-show="showReports">
+      <van-cell icon="arrow-left" @click="showReports=false"/>
+      <van-cell title="标题夸张"/>
+      <van-cell title="低俗色情"/>
+      <van-cell title="错别字多"/>
+    </van-cell-group>
+  </van-dialog>
 </template>
 
 <script>
 export default {
-  name: 'MoreAction'
+  name: 'MoreAction',
+  data () {
+    return {
+      show: true,
+      showReports: false
+    }
+  }
 }
 </script>
 
