c97be7fd990abe514caed2a80b2458b355c3ae95
piaoge <2393764821@qq.com>
2019-09-05 22:15:47 +0800

    汉化

--- src/utils/dayjs.js
+++ src/utils/dayjs.js
@@ -1,5 +1,8 @@
 import dayjs from 'dayjs'
 import relativeTime from 'dayjs/plugin/relativeTime'
+// 导入语言包
+import 'dayjs/locale/zh-cn'
+dayjs.locale('zh-cn')
 
 dayjs.extend(relativeTime)
 
@@ -8,5 +11,5 @@ export const fmtDate = (value) => {
   // console.log(dayjs())
   // dayjs() 获取当前时间（dayjs中一个特殊的对象）
   // dayjs(value) 指定的时间（dayjs中一个特殊的对象）
-  return dayjs().from(dayjs(value))
+  return dayjs().to(dayjs(value))
 }
