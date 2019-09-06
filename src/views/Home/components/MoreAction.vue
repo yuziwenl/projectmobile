8c548cf55fe4c631c3d4d86e456dc91a85f94d45
piaoge <2393764821@qq.com>
2019-09-06 21:18:39 +0800

    处理不感兴趣

--- src/views/Home/components/MoreAction.vue
+++ src/views/Home/components/MoreAction.vue
@@ -6,9 +6,10 @@
     closeOnClickOverlay
   >
    <van-cell-group v-show="!showReports">
-      <van-cell title="不感兴趣" icon="location-o"/>
+      <van-cell title="不感兴趣" icon="location-o" @click="handle('dislike')"/>
       <van-cell title="反馈垃圾内容" icon="location-o" is-link @click="showReports=true"/>
-      <van-cell title="拉黑作者" icon="location-o"/>
+      <van-cell title="拉黑作者" icon="location-o"
+      @click="handle('blacklist')"/>
     </van-cell-group>
     <!-- 举报文章 -->
     <van-cell-group v-show="showReports">
@@ -21,6 +22,7 @@
 </template>
 
 <script>
+import { dislikeArticle } from '@/api/article'
 export default {
   name: 'MoreAction',
   props: {
@@ -38,8 +40,24 @@ export default {
       showReports: false
     }
   },
-  created () {
-    console.log(this.article.art_id)
+  methods: {
+    handle (type) {
+      switch (type) {
+        case 'dislike':
+          this.dislike()
+          break
+        case 'blacklist':
+          break
+      }
+    },
+    async dislike () {
+      try {
+        await dislikeArticle(this.article.art_id)
+        this.$toast.success('操作成功')
+      } catch (err) {
+        this.$toast.fail('操作失败')
+      }
+    }
   }
 }
 </script>
