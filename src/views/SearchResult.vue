672d76d817ec2b14c216072366601e31cfb0d034
piaoge <2393764821@qq.com>
2019-09-09 19:23:54 +0800

    搭建列表组件

--- src/views/SearchResult.vue
+++ src/views/SearchResult.vue
@@ -1,16 +1,54 @@
 <template>
-   <van-nav-bar
+<div>
+<van-nav-bar
       title="搜索结果"
       left-text="返回"
       left-arrow
       @click-left="$router.back()"
     />
+      <van-list
+      v-model="loading"
+      :finished="finished"
+      finished-text="没有更多了"
+      @load="onLoad"
+    >
+      <van-cell
+        v-for="item in list"
+        :key="item"
+        :title="item"
+      />
+    </van-list>
+</div>
+
 </template>
 
 <script>
 export default {
   name: 'SearchResult',
-  props: ['q']
+  props: ['q'],
+  data () {
+    return {
+      list: [],
+      loading: false,
+      finished: false
+    }
+  },
+  methods: {
+    onLoad () {
+      // 异步更新数据
+      setTimeout(() => {
+        for (let i = 0; i < 10; i++) {
+          this.list.push(this.list.length + 1)
+        }
+        // 加载状态结束
+        this.loading = false
+        // 数据全部加载完成
+        if (this.list.length >= 40) {
+          this.finished = true
+        }
+      }, 500)
+    }
+  }
 }
 </script>
 
