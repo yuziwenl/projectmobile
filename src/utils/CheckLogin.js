36c3a78ae12fcff0d4d3d47e6ddb134b080493e8
piaoge <2393764821@qq.com>
2019-09-10 10:38:00 +0800

    跳转回来的页面

--- src/utils/CheckLogin.js
+++ src/utils/CheckLogin.js
@@ -6,7 +6,12 @@ export default {
           title: '登录提示',
           message: '是否要跳转到登录页面'
         }).then(() => {
-          this.$router.push('/login')
+          this.$router.push({
+            path: '/login',
+            query: {
+              redirect: this.$route.fullPath
+            }
+          })
         })
         return false
       }
