62f19cf86cacb7e8637eb91f979e19454626f33b
piaoge <2393764821@qq.com>
2019-09-10 17:22:30 +0800

    封装发布评论API

--- src/api/comment.js
+++ src/api/comment.js
@@ -13,3 +13,17 @@ export const getComments = ({
     limit
   } })
 }
+export const sendComment = (
+  // 评论的目标id（评论文章即为文章id，对评论进行回复则为评论id）
+  obj
+  // 评论的内容
+
+  // 文章id，对评论内容发表回复时，需要传递此参数，表明所属文章id。对文章进行评论，不要传此参数。
+
+) => {
+  return request.post('/app/v1_0/comments', {
+    target: obj.target,
+    content: obj.content,
+    art_id: obj.art_id
+  })
+}
